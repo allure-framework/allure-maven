1f5166a96963676137801f54977eaebb8a5df4af
Dmitry Baev <baev@users.noreply.github.com>
2021-10-21 19:09:48 +0300

    fix tests

--- src/it/aggregate-multi-module/verify.groovy
+++ src/it/aggregate-multi-module/verify.groovy
@@ -4,9 +4,5 @@ import static io.qameta.allure.maven.TestHelper.checkReportDirectory
 
 def basedirPath = basedir.absolutePath
 def outputDirectory = Paths.get(basedirPath, 'target', 'site', 'allure-maven-plugin')
-def firstOutputDirectory = Paths.get(basedirPath, 'first', 'target', 'site', 'allure-maven-plugin')
-def secondOutputDirectory = Paths.get(basedirPath, 'second', 'target', 'site', 'allure-maven-plugin')
 
 checkReportDirectory(outputDirectory, 2)
-checkReportDirectory(firstOutputDirectory, 1)
-checkReportDirectory(secondOutputDirectory, 1)
