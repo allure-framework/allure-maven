8d7f7ff646add566079270c40ab72316d517c4ac
Dmitry Baev <baev@users.noreply.github.com>
2026-04-15 14:50:59 +0100

    remove duplicate log

--- src/it/aggregate-multi-module-exclude-report/verify.groovy
+++ src/it/aggregate-multi-module-exclude-report/verify.groovy
@@ -1,6 +1,9 @@
 import java.nio.file.Paths
 
+import static io.qameta.allure.maven.TestHelper.checkAggregateMojoOnly
 import static io.qameta.allure.maven.TestHelper.checkReportDirectory
+
+checkAggregateMojoOnly(Paths.get(basedir.absolutePath))
 
 def outputDirectory = Paths.get(basedir.absolutePath, 'target', 'site', 'allure-maven-plugin')
 checkReportDirectory(outputDirectory, 2)
